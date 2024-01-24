f509bd37084949d9b43497c3181f03d213b6757f
pierrotkasongo <pierrot@exalt.cd>
2024-01-24 19:18:44 +0100

    first commit

---
+++ JS/recette.js
@@ -0,0 +1,88 @@
+function recette(){
+    var a = document.getElementById("search").value;
+    
+    if (a=="Mabundu" || a=="mabundu") {
+        window.open("../pages/recettes/Mabundu.html");
+    }
+    if (a=="Mboloko" || a=="mboloko") {
+        window.open("../pages/recettes/Mboloko.html");
+    }
+    if (a=="Antilope" || a=="antilope") {
+        window.open("../pages/recettes/Mboloko.html");
+    }
+    if (a=="Makayabu" || a=="makayabu") {
+        window.open("../pages/recettes/Makayabu.html");
+    }
+    if (a=="Morue" || a=="morue") {
+        window.open("../pages/recettes/Makayabu.html");
+    }
+    if (a=="Madesu" || a=="madesu") {
+        window.open("../pages/recettes/madesu.html");
+    }
+    if (a=="Haricot" || a=="haricot") {
+        window.open("../pages/recettes/madesu.html");
+    }
+    if (a=="nsaka madesu" || a=="Nsaka madesu") {
+        window.open("../pages/recettes/Nsaka.html");
+    }
+    if (a=="saka madesu" || a=="Saka madesu") {
+        window.open("../pages/recettes/Nsaka.html");
+    }
+    if (a=="Nfumbwa" || a=="Nfumbwa") {
+        window.open("../pages/recettes/Mabundu.html");
+    }
+    if (a=="Fumbwa" || a=="fumbwa") {
+        window.open("../pages/recettes/fumbwa.html");
+    }
+    if (a=="Pondu" || a=="pondu") {
+        window.open("../pages/recettes/Pondu.html");
+    }
+    if (a=="Misili" || a=="misili") {
+        window.open("../pages/recettes/misili.html");
+    }
+    if (a=="Ndunda" || a=="ndunda") {
+        window.open("../pages/recettes/Ndunda.html");
+    }
+    if (a=="Dongo-dongo" || a=="dongo-dongo") {
+        window.open("../pages/recettes/Dongo-dongo.html");
+    }
+    if (a=="Dongo dongo" || a=="dongo dongo") {
+        window.open("../pages/recettes/Dongo-dongo.html");
+    }
+    if(a=="Ngando" || a=="ngando") {
+        window.open("../pages/recettes/ngando.html");
+    } 
+    if (a=="Ngai-ngai" || a=="ngai-ngai") {
+        window.open("../pages/recettes/Ngai-ngai.html");
+    } 
+    if (a=="Ngai ngai" || a=="ngai ngai") {
+        window.open("../pages/recettes/Ngai-ngai.html");
+    }
+    if (a=="Oseille" || a=="oseille") {
+        window.open("../pages/recettes/Ngai-ngai.html");
+    }
+     if (a=="Mikungu" || a=="mikungu") {
+        window.open("../pages/recettes/mikungu.html");
+    } 
+    if (a=="Mayebo" || a=="mayebo") {
+        window.open("../pages/recettes/Mayebo.html");
+    } 
+    if (a=="Aubergine" || a=="aubergine") {
+        window.open("../pages/recettes/Auberge.html");
+    }
+    if (a=="Ngolo" || a=="ngolo") {
+        window.open("../pages/recettes/ngolo.html");
+    }
+    if(a=="Dessert" || a=="dessert"){
+        window.open("../pages/recettes/Dessert.html");
+    }
+    if(a=="Boisson" || a=="boisson"){
+        window.open("../pages/recettes/boisson.html");
+    }
+    if(a=="Viande" || a=="viande"){
+        window.open("../pages/recettes/viande.html");
+    }
+    else{
+        window.open("../pages/recettes/introu.html");
+    }
+}
